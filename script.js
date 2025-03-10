153ba34054f4fec56a273ec9c0912a9b1635fe7f
Gopal Poddar <g43142324@gmail.com>
2025-03-10 12:29:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth Scroll Animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  // Sticky Navbar
+  window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  });
