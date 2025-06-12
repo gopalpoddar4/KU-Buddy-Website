4055762d9ebe2c7e91b5bef5f135225c81f18940
Gopal Poddar <g43142324@gmail.com>
2025-06-12 14:38:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,5 +21,5 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
   document.getElementById("download").addEventListener("click",function(e){
     e.preventDefault();
-    window.location.href = "https://www.mediafire.com/file/rnsyohuuqky3qa1/KU_Buddy.apk/file"
+    window.location.href = "https://www.mediafire.com/file/zs41bmrde3d7nlp/app-release.apk/file"
   })
